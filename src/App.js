96bbbf7cdd9f3fb540ac07be232c98852f587b2e
Beatriz Motta <bigobaldohoran@gmail.com>
2021-08-04 16:37:21 -0300

    Deleting extras

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello mu name is susi</h1>
+      <h2>im xim</h2>
+      <h3>Adam Sandler</h3>
     </div>
   );
 }
